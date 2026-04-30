33de299f12fd23e6d09ff7de79c8df1281b04bd1
Trace <50584914+Merferfer@users.noreply.github.com>
2026-04-30 18:30:34 -0400

    venv, Main parser file

--- Main.py
+++ Main.py
@@ -1,1 +1,26 @@
-import stuff
+import pandas as ps
+
+
+filepath = ".\DataRepository\imports-85.data"
+
+class parser:
+
+  def parse(filepath):
+    file = open(filepath)
+
+    dataFrame = ps.read_csv(file)
+
+    return dataFrame
+
+
+  
+
+
+if __name__ == "__main__":
+
+  parser.parse(filepath)
+
+
+
+
+
