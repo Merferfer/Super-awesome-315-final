5cd1c5713cd4709a1ade9f91809e77badedccc7f
Trace <50584914+Merferfer@users.noreply.github.com>
2026-05-03 15:35:44 -0400

    Stuff

--- firstQuestion.py
+++ firstQuestion.py
@@ -1,5 +1,6 @@
 import Main
 import pandas as ps
+from matplotlib import pyplot as plt
 
 filepath = ".\DataRepository\imports-85.data"
 
@@ -7,4 +8,19 @@ parser = Main.parser
 
 allData = parser.parse(filepath)
 
-print(allData)
+##TODO
+#Possible engine properties:
+#   bore(19), stroke(20), compression ratio (21), horsepower (22), 
+#   peak rmp (23), city mpg (24), highway mpg (25)
+#   num of cylinders (16), engine size (17), fuel system (18)
+
+#Create a plot of 
+plt.hist(allData['21'],label="City MPG?")
+plt.hist(allData['27'],label="Highway MPG?")
+plt.legend()
+plt.show()
+
+
+
+print(allData.columns)
+
