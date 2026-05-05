63b7d1de750b00cb7df527b2638ab4093e927c0b
Trace <50584914+Merferfer@users.noreply.github.com>
2026-05-05 17:41:39 -0400

    Made tons of plots and saved them

--- firstQuestion.py
+++ firstQuestion.py
@@ -1,9 +1,10 @@
 import Main
 import pandas as ps
 import numpy as np
+from word2number import w2n
 from matplotlib import pyplot as plt
 
-filepath = ".\DataRepository\imports-85.data"
+filepath = r".\DataRepository\imports-85.data"
 
 parser = Main.parser
 
@@ -19,7 +20,6 @@ allData = parser.parse(filepath)
 citympg = allData["city-mpg"]
 highwaympg = allData["highway-mpg"]
 
-print((allData["num-of-cylinders"][3]))
 
 bore = parser.refineData(allData,"bore",float)
 stroke = parser.refineData(allData,"stroke",float)
@@ -30,18 +30,75 @@ engineSize = parser.refineData(allData,"engine-size",float)
 fuelSystem = parser.refineData(allData,"fuel-system",str)
 peakRPM = parser.refineData(allData,"peak-rpm",float)
 engineLoc = parser.refineData(allData,"engine-location",str)
+fuelType = parser.refineData(allData,"fuel-type",str)
+
+# sortCylinders = sorted(allData,key=w2n.word_to_num(allData["num-of-cylinders"]))
 
 
-# plt.scatter(bore,highwaympg,color='red',label="bore")
+plt.scatter(horsepower,highwaympg,color='red',label="Highway MPG")
+plt.scatter(horsepower,citympg,color='blue',label="City MPG")
+plt.xlabel("Horsepower")
+plt.ylabel("Miles Per Gallon")
+plt.title("Horsepower vs MPG")
+plt.legend()
+plt.show()
+
+plt.scatter(numCylinders[55],highwaympg[55],color='red')
+plt.scatter(numCylinders[18],highwaympg[18],color='red')
+plt.scatter(numCylinders[0],highwaympg[0],color='red')
+plt.scatter(numCylinders[4],highwaympg[4],color='red')
+plt.scatter(numCylinders[2],highwaympg[2],color='red')
+plt.scatter(numCylinders[71],highwaympg[71],color='red')
+plt.scatter(numCylinders[49],highwaympg[49],color='red')
+
+plt.scatter(numCylinders,highwaympg,color='red',label="Highway MPG")
+plt.scatter(numCylinders,citympg,color='blue',label="City MPG")
+plt.xlabel("number of cylinders")
+plt.ylabel("Miles Per Gallon")
+plt.title("Number of Cylinders vs MPG")
+plt.legend()
+plt.show()
+
+plt.scatter(engineSize,highwaympg,color='red',label="Highway MPG")
+plt.scatter(engineSize,citympg,color='blue',label="City MPG")
+plt.xlabel("Engine size (cubic inches)")
+plt.ylabel("Miles Per Gallon")
+plt.title("Engine Size vs MPG")
+plt.legend()
+plt.show()
+
+plt.scatter(stroke,highwaympg,color='red',label="Highway MPG")
+plt.scatter(stroke,citympg,color='blue',label="City MPG")
+plt.xlabel("stroke (in)")
+plt.ylabel("Miles Per Gallon")
+plt.title("Stroke vs MPG")
+plt.legend()
+plt.show()
+
+plt.scatter(fuelType,highwaympg,color='red',label="Highway MPG")
+plt.scatter(fuelType,citympg,color='blue',label="City MPG")
+plt.xlabel("Fuel Type")
+plt.ylabel("Miles Per Gallon")
+plt.title("Fuel Type vs MPG")
+plt.legend()
+plt.show()
+
+plt.scatter(peakRPM,highwaympg,color='red',label="Highway MPG")
+plt.scatter(peakRPM,citympg,color='blue',label="City MPG")
+plt.xlabel("peak RPM")
+plt.ylabel("Miles Per Gallon")
+plt.title("peak RPM vs MPG")
+plt.legend()
+plt.show()
+
 # plt.scatter(stroke,highwaympg,color='blue',label="stroke")
 # plt.scatter(compRatio,highwaympg,color='green',label="compression ratio")
 
-plt.scatter(engineLoc,highwaympg)
+# Useless: Engine Loc
+
 
 # Higher Horsepower, engine size, and number of cylinders correlate to lower mpg
 
-plt.legend()
-plt.show()
 
 
 # plt.legend()
