d116704157733fcd2ccee82d597aab585865dd97
Trace <50584914+Merferfer@users.noreply.github.com>
2026-05-05 12:22:37 -0400

    More work on Q1 and edits to Main parser fns

--- Main.py
+++ Main.py
@@ -1,23 +1,69 @@
 import pandas as ps
+from pandas import StringDtype
+import numpy as np
+from numpy import dtype
 
 
 filepath = ".\DataRepository\imports-85.data"
 
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration",'num-of-doors',
+"body-style",
+"drive-wheels",
+"engine-location",
+"wheel-base",
+"length",
+"width",
+"height",
+"curb-weight",
+"engine-type",
+"num-of-cylinders",
+"engine-size",
+"fuel-system",
+"bore",
+"stroke",
+"compression-ratio",
+"horsepower",
+"peak-rpm",
+"city-mpg",
+"highway-mpg",
+"price"]
+
 class parser:
 
   def parse(filepath):
+
     file = open(filepath)
 
-    dataFrame = ps.read_csv(file)
+
+    dataFrame = ps.read_csv(file,names=headers)
+
+    
 
     return dataFrame
+  
+# Takes the column of the dataframe, casts each element to type, 
+# and turns the missing data to None 
+  def refineData(dataFrame,header: str,typeCast: dtype):
+
+    if(typeCast == str):
+      refinedData = np.empty(len(dataFrame[header]),dtype=np.dtypes.StringDType)
+    else:
+      refinedData = np.empty(len(dataFrame[header]),dtype=typeCast)     
 
 
+    for a in range(0,len(dataFrame[header])):
+      if dataFrame[header][a] == "?":
+        refinedData[a] = None
+      else:
+        refinedData[a] = typeCast(dataFrame[header][a])
+
+    return refinedData
   
 
 
 if __name__ == "__main__":
 
-  parser.parse(filepath)
+  frame = parser.parse(filepath)
+  print(frame)
 
 
--- firstQuestion.py
+++ firstQuestion.py
@@ -1,5 +1,8 @@
+from cProfile import label
+
 import Main
 import pandas as ps
+import numpy as np
 from matplotlib import pyplot as plt
 
 filepath = ".\DataRepository\imports-85.data"
@@ -11,16 +14,40 @@ allData = parser.parse(filepath)
 ##TODO
 #Possible engine properties:
 #   bore(19), stroke(20), compression ratio (21), horsepower (22), 
-#   peak rmp (23), city mpg (24), highway mpg (25)
+#   peak rpm (23), city mpg (24), highway mpg (25)
 #   num of cylinders (16), engine size (17), fuel system (18)
 
-#Create a plot of 
-plt.hist(allData['21'],label="City MPG?")
-plt.hist(allData['27'],label="Highway MPG?")
+
+citympg = allData["city-mpg"]
+highwaympg = allData["highway-mpg"]
+
+print((allData["num-of-cylinders"][3]))
+
+bore = parser.refineData(allData,"bore",float)
+stroke = parser.refineData(allData,"stroke",float)
+compRatio = parser.refineData(allData,"compression-ratio",float)
+numCylinders = parser.refineData(allData,"num-of-cylinders",str)
+horsepower = parser.refineData(allData,"horsepower",float)
+engineSize = parser.refineData(allData,"engine-size",float)
+fuelSystem = parser.refineData(allData,"fuel-system",str)
+peakRPM = parser.refineData(allData,"peak-rpm",float)
+engineLoc = parser.refineData(allData,"engine-location",str)
+
+
+# plt.scatter(bore,highwaympg,color='red',label="bore")
+# plt.scatter(stroke,highwaympg,color='blue',label="stroke")
+# plt.scatter(compRatio,highwaympg,color='green',label="compression ratio")
+
+plt.scatter(engineLoc,highwaympg)
+
+# Higher Horsepower, engine size, and number of cylinders correlate to lower mpg
+
 plt.legend()
 plt.show()
 
 
+# plt.legend()
 
-print(allData.columns)
+
+
 
